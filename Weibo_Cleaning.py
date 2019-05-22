fe8f4106443565aa7497ea03f5bd6dd877e4f10d
Ui <ui.long@sands.com.mo>
2019-05-22 16:19:33 +0800

    update

--- Weibo_Cleaning.py
+++ Weibo_Cleaning.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import re
-
+from datetime import datetime
+now = datetime.now()
 
 def weibo_cleaning(file):
     df = pd.read_csv(file, encoding='utf-8')
@@ -13,15 +14,23 @@ def weibo_cleaning(file):
 
     df['read_number'] = df['line1_link'].apply(lambda x: read_number(x))
 
-    sep = ' 来自'
-    df['WB_detail'] = df['WB_detail'].apply(lambda x: x.split(sep, 1)[0])
-    if "今天" not in df['WB_detail']:
-        df['datetime'] = pd.to_datetime(df['WB_detail'], format='%m月%d日 %H:%M', errors='coerce')
-    else:
-        df['datetime'] = df['WB_detail']
+    import datetime
+    from datetime import datetime
+    now = datetime.now()
 
-    df['datetime'] = df['datetime'].apply(lambda x: x.replace(year=2019))
+    def change_date(datestring):
+        sep = ' 来自'
+        datestring = datestring.split(sep, 1)[0]
+        if "今天" not in datestring:
+            dateint = datetime.strptime(datestring, '%m月%d日 %H:%M')
+        else:
+            dateint = datestring
+        return dateint
+
+    df['datetime'] = df['WB_detail'].apply(lambda x: change_date(x))
+    df['datetime'] = pd.to_datetime(df['datetime'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
     df['datetime'] = pd.to_datetime(df['datetime']).dt.date
+    df['datetime'] = df['datetime'].apply(lambda x: x.replace(now.year))
 
     cols = ['line2', 'line3', 'line4']
     df[cols] = df[cols].apply(pd.to_numeric, downcast='integer', errors='coerce')
